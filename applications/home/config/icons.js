75d223e3ff847263526e38636ef078cef5ae9ca2
cityowl / jigmet <cityowl@hey.com>
2023-01-27 14:45:07 +0530

    front-end ember app config

---
+++ applications/home/config/icons.js
@@ -0,0 +1,14 @@
+module.exports = function () {
+  return {
+    'pro-light-svg-icons': [
+      'table-list',
+    ],
+    'pro-regular-svg-icons': [
+      'table-list',
+    ],
+    'pro-solid-svg-icons': [
+      'table-list',
+    ],
+    'free-brands-svg-icons': ['facebook', 'twitter', 'whatsapp', 'instagram'],
+  };
+};
